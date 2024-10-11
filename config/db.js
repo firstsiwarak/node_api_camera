b956d2635d633236a905ac021c67e887cfd5e6df
firstsiwarak <firstsiwarak@gmail.com>
2024-10-11 16:42:09 +0700

    edit db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('nodeapidb', 'postgres', '12345678', {
-host: 'localhost',
+const sequelize = new Sequelize('apinodedb_wqv5', 'nodeapi', 'bxeyYWSFsHgTTwAyE6oZujl2yG27AuTO', {
+host: 'dpg-cs4f280gph6c73c027ag-a',
 dialect: 'postgres',
 });
 
